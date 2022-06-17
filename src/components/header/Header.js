73e48a194b680d8501e731687b68c32c1e50d541
manuto276 <emanuele.frascella256@gmail.com>
2022-06-17 18:59:01 +0200

    Removed Navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import './Header.css';
 import { AccountCircle, ShoppingCart } from '../icon/Icon';
 import Logo from '../../resources/logo.svg';
-import { Link } from '../link/Link';
+import { AnimatedLink, Link } from '../link/Link';
 
 function Header(props) {
     return (
@@ -9,11 +9,6 @@ function Header(props) {
             <Link id='logo' >
                 <img src={ Logo } alt='Mc Donarudo' />
             </Link>
-            <ul className='Navigation'>
-                <li className='Button'>home</li>
-                <li className='Button'>menu</li>
-                <li className='Button'>about us</li>
-            </ul>
             <div className='More'>
                 <p>1 800 675 75 75</p>
                 <ShoppingCart id='cart' />
