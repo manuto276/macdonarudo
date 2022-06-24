6f35fbdc228539855b0d577c69b80fece4795207
manuto276 <emanuele.frascella256@gmail.com>
2022-06-24 23:52:50 +0200

    Added Menu Product Items

--- src/components/routes/menu/Menu.js
+++ src/components/routes/menu/Menu.js
@@ -1,9 +1,14 @@
 import './Menu.css';
+import './FoodCategories.css';
+import './ProductItem.css';
+
+import bbq from '../../../resources/images/bbq.png';
+
 import { useContext, useState } from 'react';
 import { FloatingActionButton } from '../../floatingactionbutton/FloatingActionButton';
 import { Footer } from '../../footer/Footer';
 import { Header } from '../../header/Header';
-import { Add } from '../../icon/Icon';
+import { Add, Delete, Edit, ShoppingCart } from '../../icon/Icon';
 import { SlideEffect } from '../../link/Link';
 import { AuthContext } from '../../../App';
 import { NewProductView } from '../../newproductview/NewProductView';
@@ -18,7 +23,7 @@ function Menu() {
     let isAdmin = authContextHook.role === 'admin';
 
     // testing 
-    isAdmin = true;
+    isAdmin = false;
 
     return (
         <section id='menu'>
@@ -27,8 +32,9 @@ function Menu() {
                 <hgroup>
                     <h1>Explore<br/>Our Menu</h1>
                 </hgroup>
-                <FoodTypeList activeIndex={activeIndex} onItemClick={(index) => setActiveIndex(index)} />
-                <div className='MenuGrid'></div>
+                <FoodCategories activeIndex={activeIndex} onItemClick={(index) => setActiveIndex(index)} />
+                <div id='menuGrid'>
+                </div>
                 {isAdmin ? 
                     <FloatingActionButton id='addFoodButton' onClick={() => setNewProductVisible(true)}>
                         <SlideEffect height='1.5rem'>
@@ -46,14 +52,47 @@ function Menu() {
 // This function returns a list of Chips components
 // which represent the food categories.
 // By clicking on them we change the category.
-function FoodTypeList(props) {
+function FoodCategories(props) {
     let activeIndex = props.activeIndex ?? 0;
     return (
-        <div className='FoodTypeList'>
+        <div className='FoodCategories'>
             {FOOD_TYPES.map((item, i) => 
                 <div className={'Chip' + (activeIndex == i ? ' Active' : '')} onClick={() => props.onItemClick(i)}>
                     <SlideEffect className='button' height='1rem'>{item}</SlideEffect>
                 </div>)}
+        </div>
+    );
+}
+
+function ProductItem(props) {
+    return (
+        <div className='ProductItem'>
+            <div className='ProductIcon'>
+                <img src={props.icon} alt={props.title} />
+            </div>
+            <div className='ProductDesc'>
+                <h6>{props.title}</h6>
+                <p>{props.price}</p>
+            </div>
+            <div className='Actions'>
+                <button>
+                    <SlideEffect height='1.5rem'>
+                        <ShoppingCart />
+                    </SlideEffect>
+                </button>
+                {props.isAdmin ? 
+                <button className='Tertiary'>
+                    <SlideEffect height='1.5rem'>
+                        <Edit />
+                    </SlideEffect>
+                </button> : null}
+                {props.isAdmin ? 
+                <button className='Tertiary'>
+                    <SlideEffect height='1.5rem'>
+                        <Delete />
+                    </SlideEffect>
+                </button> : null}
+            </div>
         </div>
     );
 }
