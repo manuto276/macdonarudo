400f418f18c0568fdce2625f71061feee2d958bc
manuto276 <emanuele.frascella256@gmail.com>
2022-06-27 23:12:56 +0200

    Updated StatusChip

--- src/components/statuschip/StatusChip.js
+++ src/components/statuschip/StatusChip.js
@@ -2,7 +2,7 @@ import './StatusChip.css';
 
 function StatusChip(props) {
     return (
-        <div className={'caption Status ' + props.status}>
+        <div className={'caption StatusChip ' + props.status}>
             {props.status === 'rejected' ? 
                 'Rejected' : props.status === 'complete' ?
                     'Complete' : props.status === 'active' ?
