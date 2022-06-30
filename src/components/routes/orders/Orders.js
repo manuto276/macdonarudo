caf522df4f9ccfbc017a46ada3be617602a4e222
Giuseppe Bosa <93953428+PeppeOsmio@users.noreply.github.com>
2022-06-30 15:26:05 +0200

    .

--- src/components/routes/orders/Orders.js
+++ src/components/routes/orders/Orders.js
@@ -136,7 +136,7 @@ function OrdersList(props) {
                 alert(error);
             });
         }else{
-            navigate('orders', {replace: true});
+            navigate('/orders', {replace: true});
         }
     }
     
