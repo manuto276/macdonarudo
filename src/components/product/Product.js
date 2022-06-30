810d9e394dd7e8a5cd86e88015605d043cdba2db
Giuseppe Bosa <93953428+PeppeOsmio@users.noreply.github.com>
2022-06-30 16:32:20 +0200

    .

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -42,11 +42,11 @@ function Product(props) {
                     <SlideEffect height='1.5rem'><Add /></SlideEffect>
                 </button>
             </div>
-            authContextHook.role === 'customer' || authContextHook.role == null ?
+            {authContextHook.role === 'customer' || authContextHook.role == null ?
             <button id='addToCartButton' onClick={addToCart}>
                 <SlideEffect className='button' height='1rem'>Add to cart</SlideEffect>
             </button>
-            : null
+            : null}
         </div>
     );
 }
